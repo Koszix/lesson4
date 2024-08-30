75412f763eac612b605e52f79e32909504156b1c
Koszix <koksyak69@gmail.com>
2024-08-30 06:44:45 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+name = 'Konstantin'
+print('Name:' + name)
+age = 24
+print('Age:' , age)
+n_age = age + 1
+print('New Age:' , n_age)
+is_student = bool(n_age > age)
+print('Is Student:' , is_student)
+
+# Почему оно легче предыдущего задания =')
